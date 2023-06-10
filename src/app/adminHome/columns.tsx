b284111bc164a4c46c10109052260b9c56afb7fb
Jakob Evangelista <jakobevangelista@tamu.edu>
2023-06-10 12:51:12 -0500

    added dialog to admin view

--- src/app/adminHome/columns.tsx
+++ src/app/adminHome/columns.tsx
@@ -6,6 +6,18 @@ import { Button } from "@/components/ui/button";
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type User = {
@@ -61,6 +73,46 @@ export const columns: ColumnDef<User>[] = [
   {
     accessorKey: "amount",
     header: "Amount",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <Dialog>
+          <DialogTrigger>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input id="name" value="Pedro Duarte" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input id="username" value="@peduarte" className="col-span-3" />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      );
+    },
   },
 ];
 
--- src/app/adminHome/data-table.tsx
+++ src/app/adminHome/data-table.tsx
@@ -142,8 +142,6 @@ export function DataTable<TData, TValue>({
         <TableBody>
           {table.getRowModel().rows?.length ? (
             table.getRowModel().rows.map((row) => (
-              // <Dialog key={row.id}>
-              //   <DialogTrigger asChild>
               <TableRow
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
@@ -154,42 +152,6 @@ export function DataTable<TData, TValue>({
                   </TableCell>
                 ))}
               </TableRow>
-              //   </DialogTrigger>
-              //   <DialogContent className="sm:max-w-[425px]">
-              //     <DialogHeader>
-              //       <DialogTitle>Edit profile</DialogTitle>
-              //       <DialogDescription>
-              //         Make changes to your profile here. Click save when you're
-              //         done.
-              //       </DialogDescription>
-              //     </DialogHeader>
-              //     <div className="grid gap-4 py-4">
-              //       <div className="grid grid-cols-4 items-center gap-4">
-              //         <Label htmlFor="name" className="text-right">
-              //           Name
-              //         </Label>
-              //         <Input
-              //           id="name"
-              //           value="Pedro Duarte"
-              //           className="col-span-3"
-              //         />
-              //       </div>
-              //       <div className="grid grid-cols-4 items-center gap-4">
-              //         <Label htmlFor="username" className="text-right">
-              //           Username
-              //         </Label>
-              //         <Input
-              //           id="username"
-              //           value="@peduarte"
-              //           className="col-span-3"
-              //         />
-              //       </div>
-              //     </div>
-              //     <DialogFooter>
-              //       <Button type="submit">Save changes</Button>
-              //     </DialogFooter>
-              //   </DialogContent>
-              // </Dialog>
             ))
           ) : (
             <TableRow>

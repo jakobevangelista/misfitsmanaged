6e77909c8b5a564a4569964775aacb3d37b11816
Jakob Evangelista <jevangelista@ITs-MacBook-Pro.local>
2023-07-27 08:45:21 -0500

    band aid fixed multi item checkout

--- src/app/api/customCheckoutSession/route.ts
+++ src/app/api/customCheckoutSession/route.ts
@@ -40,6 +40,43 @@ export async function POST(req: Request) {
     });
 
     try {
+      for (let i = 0; i < data.arg.cartItems.length; i++) {
+        if (data.arg.cartItems[i] === "price_1NYRbKD5u1cDehOfapzIEhrJ" || data.arg.cartItems[i] === "price_1NYRcWD5u1cDehOfiPRDAB3v" || data.arg.cartItems[i] === "price_1NYU4xD5u1cDehOfzRdl9hRN" || data.arg.cartItems[i] === "price_1NYRbrD5u1cDehOfLWSsrUWc") {
+          let session;
+          session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            billing_address_collection: "required",
+            customer,
+            customer_update: {
+              address: "auto",
+            },
+            line_items: data.arg.cartItems,
+
+            mode: "subscription",
+            allow_promotion_codes: false,
+            success_url: `${getURL()}/adminHome`,
+            cancel_url: `${getURL()}/adminHome`,
+          });
+
+          if (session) {
+            revalidatePath("/adminHome");
+            revalidatePath("/transactions");
+            console.log("we not seeing anything");
+            console.log(session.id);
+            return new Response(JSON.stringify({ sessionId: session.id }), {
+              status: 200,
+            });
+          } else {
+            console.log("error");
+            return new Response(
+              JSON.stringify({
+                error: { statusCode: 500, message: "Session is not defined" },
+              }),
+              { status: 500 }
+            );
+          }
+        }
+      }
       let session;
       session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
@@ -50,7 +87,7 @@ export async function POST(req: Request) {
         },
         line_items: data.arg.cartItems,
 
-        mode: "subscription",
+        mode: "payment",
         allow_promotion_codes: false,
         success_url: `${getURL()}/adminHome`,
         cancel_url: `${getURL()}/adminHome`,
@@ -73,6 +110,7 @@ export async function POST(req: Request) {
           { status: 500 }
         );
       }
+
     } catch (err: any) {
       console.log(err);
       return new Response(JSON.stringify(err), { status: 500 });

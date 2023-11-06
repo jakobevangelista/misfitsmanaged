29e0e05c6dc80669bd4d3a184059eddb7b442a15
Jakob Evangelista <jakobevangelista@tamu.edu>
2023-11-06 13:53:21 -0600

    added mobile onclick close menu on landing page

--- src/components/landingPage/clientHero.tsx
+++ src/components/landingPage/clientHero.tsx
@@ -61,6 +61,7 @@ const ClientHeroSection = () => {
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-[#efe1b2] hover:bg-gray-800"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </a>
